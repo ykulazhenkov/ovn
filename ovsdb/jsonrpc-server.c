openvswitch/ovs

commit 60e0cd0419588f520d88a71fa70df3b130e18526

ovsdb: Replication usability improvements
Based on feedbacks from initial HA manager integration, added the
'--active' command line option and appctl command
"ovsdb-server/sync-status. See man page updates for details.

Added the RPL_S_INIT state in the state machine. This state is
not strictly necessary for the replication state machine, but is
introduced to make sure the state is update immediately when
the state machine is reset, via replication_init(). Without it
ovsdb/sync-status may display "replicating" or crash, if the command
is issued between after replication_init() is called, but before
the state variable is updated from replication_run().

Added a test to simulate the integration of HA manager with OVSDB
server using replication.

Other documentation and API improvements.

Tested-by: Numan Siddique <nusiddiq@redhat.com>
Signed-off-by: Andy Zhou <azhou@ovn.org>
Acked-by: Ben Pfaff <blp@ovn.org>

ovsdb/jsonrpc-server.c
@@ -343,6 +343,12 @@ ovsdb_jsonrpc_server_reconnect(struct ovsdb_jsonrpc_server *svr, bool read_only)
     }
 }
 
+bool
+ovsdb_jsonrpc_server_is_read_only(struct ovsdb_jsonrpc_server *svr)
+{
+    return svr->read_only;
+}
+
 void
 ovsdb_jsonrpc_server_run(struct ovsdb_jsonrpc_server *svr)
 {

ovsdb/jsonrpc-server.h
@@ -65,6 +65,7 @@ void ovsdb_jsonrpc_server_reconnect(struct ovsdb_jsonrpc_server *, bool read_onl
 
 void ovsdb_jsonrpc_server_run(struct ovsdb_jsonrpc_server *);
 void ovsdb_jsonrpc_server_wait(struct ovsdb_jsonrpc_server *);
+bool ovsdb_jsonrpc_server_is_read_only(struct ovsdb_jsonrpc_server *);
 
 void ovsdb_jsonrpc_server_get_memory_usage(const struct ovsdb_jsonrpc_server *,
                                            struct simap *usage);

ovsdb/ovsdb-server.1.in
@@ -36,7 +36,7 @@ If none is specified, the default is \fB@DBDIR@/conf.db\fR.  The database
 files must already have been created and initialized using, for
 example, \fBovsdb\-tool create\fR.
 .
-.SH "ACTIVE and BACKUP "
+.SH "ACTIVE and BACKUP"
 \fBovsdb\-server\fR runs either as a backup server, or as an active server.
 When  \fBovsdb\-server\fR is running as a backup server, all transactions that
 can modify the database content, including the lock commands are rejected.
@@ -45,12 +45,12 @@ When \fBovsdb\-server\fR role changes, all existing client connection are
 reset, requiring clients to reconnect to the server.
 .PP
 By default, \fBovsdb\-server\fR runs as an active server, except when the
-\fB\-\-sync\-from=\fIserver\fR command line option is specified.  During
-runtime, \fBovsdb\-server\fR role can be switch by using appctl commands.
+\fB\-\-sync\-from=\fIserver\fR command line option is specified and without
+the \fB\-\-no\-sync option\fR.  During runtime, \fBovsdb\-server\fR role can be switch by using appctl commands.
 .PP
 \fBovsdb-server/connect\-active\-ovsdb\-server\fR switches
-\fBovsdb\-server\fR role into a backup server, Conversely,
-\fBovsdb-server/disconnect\-active\-ovsdb\-server\fR changes server into
+\fBovsdb\-server\fR into a backup server, Conversely,
+\fBovsdb-server/disconnect\-active\-ovsdb\-server\fR switches server into
 an active one.
 .
 .SH OPTIONS
@@ -220,11 +220,21 @@ specified by \fBovsdb\-server/set\-active\-ovsdb\-server\fR.
 .IP "\fBovsdb\-server/disconnect\-active\-ovsdb\-server"
 Causes \fBovsdb\-server\fR to  stop  synchronizing  its  databases with a active server.
 .
-.IP "\fBovsdb\-server/set\-sync\-excluded\-tables \fIdb\fB:\fItable\fR[\fB,\fIdb\fB:\fItable\fR]..."
+.IP "\fBovsdb\-server/set\-sync\-exclude\-tables \fIdb\fB:\fItable\fR[\fB,\fIdb\fB:\fItable\fR]..."
 Sets the \fItable\fR whitin \fIdb\fR that will be excluded from synchronization.
 .
-.IP "\fBovsdb\-server/get\-sync\-excluded\-tables"
+.IP "\fBovsdb\-server/get\-sync\-exclude\-tables"
 Gets  the  tables  that are currently excluded from synchronization.
+.
+.IP "\fBovsdb\-server/sync\-status"
+Prints a summary of replication run time information. The \fBstate\fR
+information is always provided, indicating whether the server is running
+in the \fIactive\fR or the \fIbackup\fR mode.
+When running in backup mode, replication connection status, which
+can be either \fIconnecting\fR, \fIreplicating\fR or \fIerror\fR, are shown.
+When the connection is in \fIreplicating\fR state, further output shows
+the list of databases currently replicating, and the tables that are
+excluded.
 .
 .so lib/vlog-unixctl.man
 .so lib/memory-unixctl.man

ovsdb/replication.c
@@ -37,7 +37,7 @@
 
 VLOG_DEFINE_THIS_MODULE(replication);
 
-static char *active_ovsdb_server;
+static char *sync_from;
 static struct jsonrpc_session *session = NULL;
 static unsigned int session_seqno = UINT_MAX;
 
@@ -87,6 +87,7 @@ static void request_ids_destroy(void);
 void request_ids_clear(void);
 
 enum ovsdb_replication_state {
+    RPL_S_INIT,
     RPL_S_DB_REQUESTED,
     RPL_S_SCHEMA_REQUESTED,
     RPL_S_MONITOR_REQUESTED,
@@ -108,8 +109,15 @@ static struct ovsdb* find_db(const char *db_name);
 
 
 void
-replication_init(void)
+replication_init(const char *sync_from_, const char *exclude_tables)
 {
+    free(sync_from);
+    sync_from = xstrdup(sync_from_);
+    char *err = set_blacklist_tables(exclude_tables, false);
+    /* Caller should have verified that the 'exclude_tables' is
+     * parseable. An error here is unexpected. */
+    ovs_assert(!err);
+
     shash_destroy(replication_dbs);
     replication_dbs = NULL;
 
@@ -118,8 +126,9 @@ replication_init(void)
         jsonrpc_session_close(session);
     }
 
-    session = jsonrpc_session_open(active_ovsdb_server, true);
+    session = jsonrpc_session_open(sync_from, true);
     session_seqno = UINT_MAX;
+    state = RPL_S_INIT;
 }
 
 void
@@ -143,7 +152,7 @@ replication_run(void)
         unsigned int seqno;
 
         seqno = jsonrpc_session_get_seqno(session);
-        if (seqno != session_seqno) {
+        if (seqno != session_seqno || state == RPL_S_INIT) {
             session_seqno = seqno;
             request_ids_clear();
             struct jsonrpc_msg *request;
@@ -156,6 +165,7 @@ replication_run(void)
             replication_dbs = replication_db_clone(&local_dbs);
 
             state = RPL_S_DB_REQUESTED;
+            VLOG_DBG("Send list_dbs request");
         }
 
         msg = jsonrpc_session_recv(session);
@@ -216,6 +226,7 @@ replication_run(void)
                             }
                         }
                     }
+                    VLOG_DBG("Send schema requests");
                     state = RPL_S_SCHEMA_REQUESTED;
                 }
                 break;
@@ -271,6 +282,7 @@ replication_run(void)
 
                             request_ids_add(request->id, db);
                             jsonrpc_session_send(session, request);
+                            VLOG_DBG("Send monitor requests");
                             state = RPL_S_MONITOR_REQUESTED;
                         }
                     }
@@ -289,6 +301,7 @@ replication_run(void)
                     /* Transition to replicating state after receiving
                      * all replies of "monitor" requests. */
                     if (hmap_is_empty(&request_ids)) {
+                        VLOG_DBG("Listening to monitor updates");
                         state = RPL_S_REPLICATING;
                     }
                 }
@@ -299,6 +312,7 @@ replication_run(void)
                 /* Ignore all messages */
                 break;
 
+            case RPL_S_INIT:
             case RPL_S_REPLICATING:
             default:
                 OVS_NOT_REACHED();
@@ -316,18 +330,6 @@ replication_wait(void)
         jsonrpc_session_wait(session);
         jsonrpc_session_recv_wait(session);
     }
-}
-
-void
-set_active_ovsdb_server(const char *active_server)
-{
-    active_ovsdb_server = nullable_xstrdup(active_server);
-}
-
-const char *
-get_active_ovsdb_server(void)
-{
-    return active_ovsdb_server;
 }
 
 /* Parse 'blacklist' to rebuild 'blacklist_tables'.  If 'dryrun' is false, the
@@ -457,9 +459,9 @@ replication_destroy(void)
     blacklist_tables_clear();
     shash_destroy(&blacklist_tables);
 
-    if (active_ovsdb_server) {
-        free(active_ovsdb_server);
-        active_ovsdb_server = NULL;
+    if (sync_from) {
+        free(sync_from);
+        sync_from = NULL;
     }
 
     request_ids_destroy();
@@ -855,12 +857,58 @@ replication_get_last_error(void)
     return err;
 }
 
+char *
+replication_status(void)
+{
+    bool alive = session && jsonrpc_session_is_alive(session);
+    struct ds ds = DS_EMPTY_INITIALIZER;
+
+    if (alive) {
+        switch(state) {
+        case RPL_S_INIT:
+        case RPL_S_DB_REQUESTED:
+        case RPL_S_SCHEMA_REQUESTED:
+        case RPL_S_MONITOR_REQUESTED:
+            ds_put_format(&ds, "connecting: %s", sync_from);
+            break;
+        case RPL_S_REPLICATING: {
+            struct shash_node *node;
+
+            ds_put_format(&ds, "replicating: %s\n", sync_from);
+            ds_put_cstr(&ds, "database:");
+            SHASH_FOR_EACH (node, replication_dbs) {
+                ds_put_format(&ds, " %s,", node->name);
+            }
+            ds_chomp(&ds, ',');
+
+            if (!shash_is_empty(&blacklist_tables)) {
+                ds_put_char(&ds, '\n');
+                ds_put_cstr(&ds, "exclude: ");
+                ds_put_and_free_cstr(&ds, get_blacklist_tables());
+            }
+            break;
+        }
+        case RPL_S_ERR:
+            ds_put_format(&ds, "Replication to (%s) failed\n", sync_from);
+            break;
+        default:
+            OVS_NOT_REACHED();
+            break;
+        }
+    } else {
+        ds_put_format(&ds, "not connected to %s", sync_from);
+    }
+    return ds_steal_cstr(&ds);
+}
+
 void
 replication_usage(void)
 {
     printf("\n\
 Syncing options:\n\
-  --sync-from=SERVER      sync DATABASE from active SERVER\n\
+  --sync-from=SERVER      sync DATABASE from active SERVER and start in\n\
+                          backup mode (except with --active)\n\
   --sync-exclude-tables=DB:TABLE,...\n\
-                          exclude the TABLE in DB from syncing\n");
+                          exclude the TABLE in DB from syncing\n\
+  --active                with --sync-from, start in active mode\n");
 }

ovsdb/replication.h
@@ -21,7 +21,30 @@
 #include <stdbool.h>
 struct ovsdb;
 
-void replication_init(void);
+/* Replication module runs when OVSDB server runs in the backup mode.
+ *
+ * API Usage
+ *===========
+ *
+ * - replication_init() needs to be called whenever OVSDB server switches into
+ *   the backup mode.
+ *
+ * - replication_add_local_db() should be called immediately after to add all
+ *   known database that OVSDB server owns, one at a time.
+ *
+ * - replication_destroy() should be called when OVSDB server shutdown to
+ *   reclaim resources.
+ *
+ * - replication_run(), replication_wait(), replication_is_alive() and
+ *   replication_get_last_error() should be call within the main loop
+ *   whenever OVSDB server runs in the backup mode.
+ *
+ *  - set_blacklist_tables(), get_blacklist_tables(),
+ *    disconnect_active_server() and replication_usage() are support functions
+ *    used mainly by uinxctl commands.
+ */
+
+void replication_init(const char *sync_from, const char *exclude_tables);
 void replication_run(void);
 void replication_wait(void);
 void replication_destroy(void);
@@ -29,9 +52,8 @@ void replication_usage(void);
 void replication_add_local_db(const char *databse, struct ovsdb *db);
 bool replication_is_alive(void);
 int replication_get_last_error(void);
+char *replication_status(void);
 
-void set_active_ovsdb_server(const char *remote_server);
-const char *get_active_ovsdb_server(void);
 char *set_blacklist_tables(const char *blacklist, bool dryrun)
     OVS_WARN_UNUSED_RESULT;
 char *get_blacklist_tables(void) OVS_WARN_UNUSED_RESULT;

ovsdb/replication.man
@@ -2,7 +2,22 @@ The following options allow \fBovsdb\-server\fR to synchronize  its  databases
 with another running \fBovsdb\-server\fR.
 .TP
 \fB\-\-sync\-from=\fIserver\fR
-Causes  \fBovsdb\-server\fR to synchronize its databases with the databases in
-\fIserver\fR.  Every transaction committed by \fIserver\fR  will  be  replicated
-to \fBovsdb\-server\fR. \fIserver\fR is an active connection method in one of
-the forms documented in \fBovsdb\-client(1).
+Sets up \fBovsdb\-server\fR to synchronize its databases with the
+databases in \fIserver\fR, which must be an active connection method
+in one of the forms documented in \fBovsdb\-client\fR(1).  Every
+transaction committed by \fIserver\fR will be replicated to
+\fBovsdb\-server\fR.  This option makes \fBovsdb\-server\fR start
+as a backup server; add \fB\-\-active\fR to make it start as an
+active server.
+.TP
+\fB\-\-sync\-exclude-tables=\fIdb:table[,db:table]...\fR
+Causes the specified tables to be excluded from replication.
+.TP
+\fB\-\-active\fR
+By default, \fB\-\-sync\-from\fR makes \fBovsdb\-server\fR start up as
+a backup for \fIserver\fR.  With \fB\-\-active\fR, however,
+\fBovsdb\-server\fR starts as an active server.  Use this option to
+allow the syncing options to be specified using command line options,
+yet start the server, as the default, active server.  To switch the
+running server to backup mode, use \fBovs-appctl(1)\fR to execute the
+\fBovsdb\-server/connect\-active\-ovsdb\-server\fR command.

tests/ovsdb-server.at
@@ -1100,22 +1100,22 @@ AT_CHECK([ovs-appctl -t ovsdb-server ovsdb-server/get-active-ovsdb-server],
 ])
 AT_CLEANUP
 
-#ovsdb-server/get-sync-excluded-tables command
-AT_SETUP([ovsdb-server/get-sync-excluded-tables])
-AT_KEYWORDS([ovsdb server replication get-excluded-tables])
+#ovsdb-server/get-sync-exclude-tables command
+AT_SETUP([ovsdb-server/get-sync-exclude-tables])
+AT_KEYWORDS([ovsdb server replication get-exclude-tables])
 ordinal_schema > schema
 AT_CHECK([ovsdb-tool create db schema], [0], [ignore], [ignore])
 on_exit 'kill `cat *.pid`'
 AT_CHECK([ovsdb-server --detach --no-chdir --pidfile --sync-exclude-tables=mydb:db1,mydb:db2 db])
 
-AT_CHECK([ovs-appctl -t ovsdb-server ovsdb-server/get-sync-excluded-tables],
+AT_CHECK([ovs-appctl -t ovsdb-server ovsdb-server/get-sync-exclude-tables],
   [0], [mydb:db1,mydb:db2
 ])
 AT_CLEANUP
 
-#ovsdb-server/set-sync-excluded-tables command
-AT_SETUP([ovsdb-server/set-sync-excluded-tables])
-AT_KEYWORDS([ovsdb server replication set-excluded-tables])
+#ovsdb-server/set-sync-exclude-tables command
+AT_SETUP([ovsdb-server/set-sync-exclude-tables])
+AT_KEYWORDS([ovsdb server replication set-exclude-tables])
 replication_schema > schema
 AT_CHECK([ovsdb-tool create db1 schema], [0], [stdout], [ignore])
 AT_CHECK([ovsdb-tool create db2 schema], [0], [stdout], [ignore])
@@ -1126,7 +1126,7 @@ on_exit 'test ! -e pid || kill `cat pid`'
 AT_CHECK([ovsdb-server --detach --no-chdir --log-file=ovsdb-server2.log --pidfile="`pwd`"/pid2 --remote=punix:db2.sock --unixctl="`pwd`"/unixctl2 --sync-from=unix:db.sock db2], [0], [ignore], [ignore])
 on_exit 'test ! -e pid2 || kill `cat pid2`'
 
-AT_CHECK([ovs-appctl -t "`pwd`"/unixctl2 ovsdb-server/set-sync-excluded-tables mydb:b], [0], [ignore], [ignore], [test ! -e pid || kill `cat pid`; test ! -e pid2 || kill `cat pid2`])
+AT_CHECK([ovs-appctl -t "`pwd`"/unixctl2 ovsdb-server/set-sync-exclude-tables mydb:b], [0], [ignore], [ignore], [test ! -e pid || kill `cat pid`; test ! -e pid2 || kill `cat pid2`])
 
 AT_CHECK([ovsdb-client transact unix:db.sock \
  '[["mydb",
@@ -1173,6 +1173,11 @@ on_exit 'test ! -e pid || kill `cat pid`'
 
 AT_CHECK([ovsdb-server --detach --no-chdir --log-file=ovsdb-server2.log --pidfile="`pwd`"/pid2 --remote=punix:db2.sock --unixctl="`pwd`"/unixctl2 db2], [0], [ignore], [ignore])
 on_exit 'test ! -e pid2 || kill `cat pid2`'
+
+dnl Try to connect without specifying the active server.
+AT_CHECK([ovs-appctl -t "`pwd`"/unixctl2 ovsdb-server/connect-active-ovsdb-server], [0],
+[Unable to connect: active server is not specified.
+], [ignore])
 
 AT_CHECK([ovs-appctl -t "`pwd`"/unixctl2 ovsdb-server/set-active-ovsdb-server unix:db.sock], [0], [stdout], [ignore])
 
@@ -1277,4 +1282,88 @@ _uuid                                name number
 
 OVSDB_SERVER_SHUTDOWN
 OVSDB_SERVER_SHUTDOWN2
+AT_CLEANUP
+
+#ovsdb-server/active-backup-role-switching
+AT_SETUP([ovsdb-server/active-backup-role-switching])
+AT_KEYWORDS([ovsdb server replication active-backup-switching])
+replication_schema > schema
+AT_CHECK([ovsdb-tool create db1 schema], [0], [stdout], [ignore])
+AT_CHECK([ovsdb-tool create db2 schema], [0], [stdout], [ignore])
+
+dnl Add some data to both DBs
+AT_CHECK([ovsdb-tool transact db1 \
+'[["mydb",
+  {"op": "insert",
+   "table": "a",
+   "row": {"number": 9, "name": "nine"}}]]'], [0], [ignore], [ignore])
+
+AT_CHECK([ovsdb-tool transact db2 \
+'[["mydb",
+  {"op": "insert",
+   "table": "a",
+   "row": {"number": 9, "name": "nine"}}]]'], [0], [ignore], [ignore])
+
+dnl Start both 'db1' and 'db2' in backup mode. Let them backup from each
+dnl other. This is not an supported operation state, but to simulate a start
+dnl up condition where an HA manger can select which one to be an active
+dnl server soon after.
+AT_CHECK([ovsdb-server --detach --no-chdir --log-file=ovsdb-server1.log --pidfile="`pwd`"/pid --remote=punix:db.sock --unixctl="`pwd`"/unixctl db1 --sync-from=unix:db2.sock --active ], [0], [ignore], [ignore])
+on_exit 'test ! -e pid || kill `cat pid`'
+
+AT_CHECK([ovs-appctl -t "`pwd`"/unixctl ovsdb-server/connect-active-ovsdb-server])
+
+AT_CHECK([ovsdb-server --detach --no-chdir --log-file=ovsdb-server2.log --pidfile="`pwd`"/pid2 --remote=punix:db2.sock --unixctl="`pwd`"/unixctl2 --sync-from=unix:db.sock db2], [0], [ignore], [ignore])
+on_exit 'test ! -e pid2 || kill `cat pid2`'
+
+dnl
+dnl make sure both servers reached the replication state
+OVS_WAIT_UNTIL([ovs-appctl -t "`pwd`"/unixctl ovsdb-server/sync-status |grep replicating])
+OVS_WAIT_UNTIL([ovs-appctl -t "`pwd`"/unixctl2 ovsdb-server/sync-status |grep replicating])
+
+dnl Switch the 'db1' to active
+AT_CHECK([ovs-appctl -t "`pwd`"/unixctl ovsdb-server/disconnect-active-ovsdb-server])
+AT_CHECK([ovs-appctl -t "`pwd`"/unixctl ovsdb-server/sync-status], [0], [state: active
+])
+
+dnl Issue a transaction to 'db1'
+AT_CHECK([ovsdb-client transact unix:db.sock \
+'[["mydb",
+  {"op": "insert",
+   "table": "a",
+   "row": {"number": 0, "name": "zero"}}]]'], [0], [ignore])
+
+dnl It should be replicated to 'db2'
+OVS_WAIT_UNTIL([ovsdb-client dump unix:db2.sock | grep zero])
+
+dnl Flip the role of 'db1' and 'db2'.  'db1' becomes backup, and db2 becomes active
+AT_CHECK([ovs-appctl -t "`pwd`"/unixctl2 ovsdb-server/disconnect-active-ovsdb-server])
+AT_CHECK([ovs-appctl -t "`pwd`"/unixctl ovsdb-server/connect-active-ovsdb-server])
+
+dnl Verify the change happend
+OVS_WAIT_UNTIL([ovs-appctl -t "`pwd`"/unixctl ovsdb-server/sync-status |grep replicating])
+AT_CHECK([ovs-appctl -t "`pwd`"/unixctl2 ovsdb-server/sync-status], [0], [state: active
+])
+
+dnl Issue an transaction to 'db2' which is now active.
+AT_CHECK([ovsdb-client transact unix:db2.sock \
+'[["mydb",
+  {"op": "insert",
+   "table": "b",
+   "row": {"number": 1, "name": "one"}}]]'], [0], [ignore])
+
+dnl The transaction should be replicated to 'db1'
+OVS_WAIT_UNTIL([ovsdb-client dump unix:db.sock | grep one])
+
+dnl Both servers should have the same content.
+AT_CHECK([ovsdb-client dump unix:db.sock], [0], [stdout])
+cat stdout > dump1
+
+AT_CHECK([ovsdb-client dump unix:db2.sock], [0], [stdout])
+cat stdout > dump2
+
+AT_CHECK([diff dump1 dump2])
+
+dnl OVSDB_SERVER_SHUTDOWN
+dnl OVSDB_SERVER_SHUTDOWN2
 AT_CLEANUP

ykulazhenkov/ovn

commit abfcd41fbc737153d100f3b6b9aadd88b078d85f

ovsdb-monitor: Fix valgrind 'possible loss' warnings.
By placing these nodes at the start of their respective structures,
several "possibly lost" warnings from valgrind are silenced.

Fixes: 60e0cd041958 ("ovsdb: Replication usability improvements")
Signed-off-by: Joe Stringer <joe@ovn.org>
Acked-by: Ben Pfaff <blp@ovn.org>

ovsdb/jsonrpc-server.c
@@ -1102,9 +1102,9 @@ ovsdb_jsonrpc_trigger_complete_done(struct ovsdb_jsonrpc_session *s)
 
 /* Jsonrpc front end monitor. */
 struct ovsdb_jsonrpc_monitor {
+    struct hmap_node node;      /* In ovsdb_jsonrpc_session's "monitors". */
     struct ovsdb_jsonrpc_session *session;
     struct ovsdb *db;
-    struct hmap_node node;      /* In ovsdb_jsonrpc_session's "monitors". */
     struct json *monitor_id;
     struct ovsdb_monitor *dbmon;
     uint64_t unflushed;         /* The first transaction that has not been

ovsdb/monitor.c
@@ -86,8 +86,8 @@ struct ovsdb_monitor_json_cache_node {
 };
 
 struct jsonrpc_monitor_node {
-    struct ovsdb_jsonrpc_monitor *jsonrpc_monitor;
     struct ovs_list node;
+    struct ovsdb_jsonrpc_monitor *jsonrpc_monitor;
 };
 
 /* A particular column being monitored. */
@@ -116,12 +116,12 @@ struct ovsdb_monitor_row {
  * 'transaction' stores the first update's transaction id.
  * */
 struct ovsdb_monitor_changes {
+    struct hmap_node hmap_node;  /* Element in ovsdb_monitor_tables' changes
+                                    hmap.  */
     struct ovsdb_monitor_table *mt;
     struct hmap rows;
     int n_refs;
     uint64_t transaction;
-    struct hmap_node hmap_node;  /* Element in ovsdb_monitor_tables' changes
-                                    hmap.  */
 };
 
 /* A particular table being monitored. */