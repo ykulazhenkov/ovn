openvswitch/ovs

commit 7777d53d2f4b84cc77868f2a55d66417fe6058d0

m4: Add hard requirements for python in "configure"
Since Python 2.7 and python-six are needed to build Open vSwitch,
./configure should return an error if they are missing or if they are too old

Signed-off-by: Timothy Redaelli <tredaelli@redhat.com>
Signed-off-by: Ben Pfaff <blp@ovn.org>
Tested-by: Timothy Redaelli <tredaelli@redhat.com>
Tested-at: https://mail.openvswitch.org/pipermail/ovs-dev/2017-April/330565.html

m4/openvswitch.m4
@@ -341,22 +341,26 @@ else:
             fi
           done
         done
-        if test $ovs_cv_python != no; then
-          if test -x "$ovs_cv_python" && ! "$ovs_cv_python" -c 'import six' >/dev/null 2>&1; then
-            ovs_cv_python=no
-            AC_MSG_WARN([Missing Python six library.])
-          fi
-        fi
       fi])
-   AC_SUBST([HAVE_PYTHON])
-   AM_MISSING_PROG([PYTHON], [python])
-   if test $ovs_cv_python != no; then
-     PYTHON=$ovs_cv_python
-     HAVE_PYTHON=yes
-   else
-     HAVE_PYTHON=no
+
+   # Set $PYTHON from cache variable.
+   if test $ovs_cv_python = no; then
+     AC_MSG_ERROR([cannot find python 2.7 or higher.])
    fi
-   AM_CONDITIONAL([HAVE_PYTHON], [test "$HAVE_PYTHON" = yes])])
+   AM_MISSING_PROG([PYTHON], [python])
+   PYTHON=$ovs_cv_python
+
+   # HAVE_PYTHON is always true.  (Python has not always been a build
+   # requirement, so this variable is now obsolete.)
+   AC_SUBST([HAVE_PYTHON])
+   HAVE_PYTHON=yes
+   AM_CONDITIONAL([HAVE_PYTHON], [test "$HAVE_PYTHON" = yes])
+
+   AC_MSG_CHECKING([whether $PYTHON has six library])
+   if ! $PYTHON -c 'import six ; six.moves.range' >AS_MESSAGE_LOG_FD 2>&1; then
+     AC_MSG_ERROR([Missing Python six library or version too old.])
+   fi
+   AC_MSG_RESULT([yes])])
 
 dnl Checks for Python 3.x, x >= 4.
 AC_DEFUN([OVS_CHECK_PYTHON3],

ykulazhenkov/ovn

commit 170d739c549c3166a606e3b42b223e034daefde7

m4: fix use of log fd vs. file in "configure"
Recent Travis builds are failing when making the distcleancheck
target with:
     ERROR: files left in build directory after distclean:
     ./5

Fix by treating log fd as a file descriptor instead of a file.

Fixes: 7777d53d2f4b ("m4: Add hard requirements for python in "configure"")
Signed-off-by: Lance Richardson <lrichard@redhat.com>
Signed-off-by: Ben Pfaff <blp@ovn.org>

m4/openvswitch.m4
@@ -357,7 +357,7 @@ else:
    AM_CONDITIONAL([HAVE_PYTHON], [test "$HAVE_PYTHON" = yes])
 
    AC_MSG_CHECKING([whether $PYTHON has six library])
-   if ! $PYTHON -c 'import six ; six.moves.range' >AS_MESSAGE_LOG_FD 2>&1; then
+   if ! $PYTHON -c 'import six ; six.moves.range' >&AS_MESSAGE_LOG_FD 2>&1; then
      AC_MSG_ERROR([Missing Python six library or version too old.])
    fi
    AC_MSG_RESULT([yes])])