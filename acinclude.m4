openvswitch/ovs

commit 27d41afaa446b582fe3f14ac0482b0bbdf5ea5b1

acinclude.m4: Avoid echo -n
-n option for echo is not portable.  Use printf instead.
This fixes OSX build on travis-ci.

Acked-by: Ben Pfaff <blp@ovn.org>
Signed-off-by: YAMAMOTO Takashi <yamamoto@ovn.org>

acinclude.m4
@@ -968,7 +968,7 @@ dnl ctags ignores symbols with extras identifiers. This builds a list of
 dnl specially handled identifiers to be ignored.
 AC_DEFUN([OVS_CTAGS_IDENTIFIERS],
     AC_SUBST([OVS_CTAGS_IDENTIFIERS_LIST],
-           [`echo -n '-I "'; sed -n 's/^#define \(OVS_[A-Z_]\+\)(\.\.\.)$/\1+/p' ${srcdir}/include/openvswitch/compiler.h  | tr \\\n ' ' ; echo -n '"'`] ))
+           [`printf '-I "'; sed -n 's/^#define \(OVS_[A-Z_]\+\)(\.\.\.)$/\1+/p' ${srcdir}/include/openvswitch/compiler.h  | tr \\\n ' ' ; printf '"'`] ))
 
 dnl OVS_PTHREAD_SET_NAME
 dnl

ykulazhenkov/ovn

commit 86032c46e8006c4f15514ff4eeb1649c94735283

acinclude.m4: Avoid error from printf.
GNU (at least) printf interprets -I as an option, but we want to print it
literally, so use %s.

CC: YAMAMOTO Takashi <yamamoto@ovn.org>
Fixes: 27d41afaa446 ("acinclude.m4: Avoid echo -n")
Signed-off-by: Ben Pfaff <blp@ovn.org>
Acked-by: Justin Pettit <jpettit@ovn.org>

acinclude.m4
@@ -968,7 +968,7 @@ dnl ctags ignores symbols with extras identifiers. This builds a list of
 dnl specially handled identifiers to be ignored.
 AC_DEFUN([OVS_CTAGS_IDENTIFIERS],
     AC_SUBST([OVS_CTAGS_IDENTIFIERS_LIST],
-           [`printf '-I "'; sed -n 's/^#define \(OVS_[A-Z_]\+\)(\.\.\.)$/\1+/p' ${srcdir}/include/openvswitch/compiler.h  | tr \\\n ' ' ; printf '"'`] ))
+           [`printf %s '-I "'; sed -n 's/^#define \(OVS_[A-Z_]\+\)(\.\.\.)$/\1+/p' ${srcdir}/include/openvswitch/compiler.h  | tr \\\n ' ' ; printf '"'`] ))
 
 dnl OVS_PTHREAD_SET_NAME
 dnl