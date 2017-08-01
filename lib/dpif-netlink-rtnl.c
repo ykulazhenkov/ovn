openvswitch/ovs

commit c33c412fb139b1fe86acbba0fb36665d6ad14d2b

dpif-netlink-rtnl: Support VXLAN-GPE
Add support for creating VXLAN tunnels with GPE. This enables layer3
VXLAN tunnels with kernel datapath.

Signed-off-by: Eric Garver <e@erig.me>
Signed-off-by: Joe Stringer <joe@ovn.org>

lib/dpif-netlink-rtnl.c
@@ -33,12 +33,13 @@ VLOG_DEFINE_THIS_MODULE(dpif_netlink_rtnl);
 #ifndef IFLA_VXLAN_MAX
 #define IFLA_VXLAN_MAX 0
 #endif
-#if IFLA_VXLAN_MAX < 25
+#if IFLA_VXLAN_MAX < 27
 #define IFLA_VXLAN_LEARNING 7
 #define IFLA_VXLAN_PORT 15
 #define IFLA_VXLAN_UDP_ZERO_CSUM6_RX 20
 #define IFLA_VXLAN_GBP 23
 #define IFLA_VXLAN_COLLECT_METADATA 25
+#define IFLA_VXLAN_GPE 27
 #endif
 
 #ifndef IFLA_GRE_MAX
@@ -69,6 +70,7 @@ static const struct nl_policy vxlan_policy[] = {
     [IFLA_VXLAN_LEARNING] = { .type = NL_A_U8 },
     [IFLA_VXLAN_UDP_ZERO_CSUM6_RX] = { .type = NL_A_U8 },
     [IFLA_VXLAN_PORT] = { .type = NL_A_U16 },
+    [IFLA_VXLAN_GPE] = { .type = NL_A_FLAG },
 };
 static const struct nl_policy gre_policy[] = {
     [IFLA_GRE_COLLECT_METADATA] = { .type = NL_A_FLAG },
@@ -171,7 +173,9 @@ dpif_netlink_rtnl_vxlan_verify(const struct netdev_tunnel_config *tnl_cfg,
             || (tnl_cfg->dst_port
                 != nl_attr_get_be16(vxlan[IFLA_VXLAN_PORT]))
             || (tnl_cfg->exts & (1 << OVS_VXLAN_EXT_GBP)
-                && !nl_attr_get_flag(vxlan[IFLA_VXLAN_GBP]))) {
+                && !nl_attr_get_flag(vxlan[IFLA_VXLAN_GBP]))
+            || (tnl_cfg->exts & (1 << OVS_VXLAN_EXT_GPE)
+                && !nl_attr_get_flag(vxlan[IFLA_VXLAN_GPE]))) {
             err = EINVAL;
         }
     }
@@ -288,6 +292,9 @@ dpif_netlink_rtnl_create(const struct netdev_tunnel_config *tnl_cfg,
         nl_msg_put_u8(&request, IFLA_VXLAN_UDP_ZERO_CSUM6_RX, 1);
         if (tnl_cfg->exts & (1 << OVS_VXLAN_EXT_GBP)) {
             nl_msg_put_flag(&request, IFLA_VXLAN_GBP);
+        }
+        if (tnl_cfg->exts & (1 << OVS_VXLAN_EXT_GPE)) {
+            nl_msg_put_flag(&request, IFLA_VXLAN_GPE);
         }
         nl_msg_put_be16(&request, IFLA_VXLAN_PORT, tnl_cfg->dst_port);
         break;

ykulazhenkov/ovn

commit 73dc39560b7beba704d428af8cc6bbe3d1cae72b

dpif-netlink-rtnl: Fix VXLAN port create for regular VXLAN
When VXLAN-GPE was introduced we added IFLA_VXLAN_GPE to the policy
parsing, but did not mark it as optional. The kernel only returns this
netlink attribute if it's actually configured.

This also adds a missing entry for IFLA_VXLAN_GBP. Apparently we have no
system-traffic test coverage there.

Fixes: c33c412fb139 ("dpif-netlink-rtnl: Support VXLAN-GPE")
Fixes: 825e45e0109f ("dpif-netlink-rtnl: Support VXLAN creation")
Reported-by: Joe Stringer <joe@ovn.org>
Signed-off-by: Eric Garver <e@erig.me>
Signed-off-by: Joe Stringer <joe@ovn.org>

lib/dpif-netlink-rtnl.c
@@ -70,7 +70,8 @@ static const struct nl_policy vxlan_policy[] = {
     [IFLA_VXLAN_LEARNING] = { .type = NL_A_U8 },
     [IFLA_VXLAN_UDP_ZERO_CSUM6_RX] = { .type = NL_A_U8 },
     [IFLA_VXLAN_PORT] = { .type = NL_A_U16 },
-    [IFLA_VXLAN_GPE] = { .type = NL_A_FLAG },
+    [IFLA_VXLAN_GBP] = { .type = NL_A_FLAG, .optional = true },
+    [IFLA_VXLAN_GPE] = { .type = NL_A_FLAG, .optional = true },
 };
 static const struct nl_policy gre_policy[] = {
     [IFLA_GRE_COLLECT_METADATA] = { .type = NL_A_FLAG },