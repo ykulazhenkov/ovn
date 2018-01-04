openvswitch/ovs

commit 2301f7ebc15e7f46a73d5c4638eec221cc9d57d4

tunnel: Make tnl_port_receive() parameter 'const'.
This function no longer has much need to modify its argument, because the
caller can now easily do the modification itself, so this commit makes
that change.

Signed-off-by: Ben Pfaff <blp@nicira.com>

ofproto/tunnel.c
@@ -159,17 +159,14 @@ tnl_port_del(struct tnl_port *tnl_port)
     }
 }
 
-/* Transforms 'flow' so that it appears to have been received by a tunnel
- * OpenFlow port controlled by this module instead of the datapath port it
- * actually came in on.  Sets 'flow''s in_port to the appropriate OpenFlow port
- * number.  Returns the 'ofport' corresponding to the new in_port.
+/* Looks in the table of tunnels for a tunnel matching the metadata in 'flow'.
+ * Returns the 'ofport' corresponding to the new in_port, or a null pointer if
+ * none is found.
  *
  * Callers should verify that 'flow' needs to be received by calling
- * tnl_port_should_receive() before this function.
- *
- * Leaves 'flow' untouched and returns null if unsuccessful. */
+ * tnl_port_should_receive() before this function. */
 const struct ofport *
-tnl_port_receive(struct flow *flow)
+tnl_port_receive(const struct flow *flow)
 {
     char *pre_flow_str = NULL;
     struct tnl_port *tnl_port;
@@ -195,9 +192,6 @@ tnl_port_receive(struct flow *flow)
     if (!VLOG_DROP_DBG(&dbg_rl)) {
         pre_flow_str = flow_to_string(flow);
     }
-
-    flow->in_port = tnl_port->ofport->ofp_port;
-    /* Keep flow->tunnel to allow matching on tunnel metadata */
 
     if (pre_flow_str) {
         char *post_flow_str = flow_to_string(flow);

ofproto/ofproto-dpif.c
@@ -3927,6 +3927,7 @@ ofproto_receive(const struct dpif_backer *backer, struct ofpbuf *packet,
             flow->in_port = OFPP_NONE;
             goto exit;
         }
+        flow->in_port = ofport->ofp_port;
         port = ofport_dpif_cast(ofport);
 
         /* XXX: Since the tunnel module is not scoped per backer, it's

ofproto/tunnel.h
@@ -37,7 +37,7 @@ bool tnl_port_reconfigure(const struct ofport *, uint32_t odp_port,
 struct tnl_port *tnl_port_add(const struct ofport *, uint32_t odp_port);
 void tnl_port_del(struct tnl_port *);
 
-const struct ofport *tnl_port_receive(struct flow *);
+const struct ofport *tnl_port_receive(const struct flow *);
 uint32_t tnl_port_send(const struct tnl_port *, struct flow *);
 
 /* Returns true if 'flow' should be submitted to tnl_port_receive(). */

ykulazhenkov/ovn

commit 68446d5962cd4a7bd134919a0890af055243e67a

tunnel: Log sanely in tnl_port_receive().
When this function was introduced in 2012, it modified its 'flow' argument
and logged the changes (at debug level).  However, since 2013 it has no
longer modified its 'flow' argument, but the logging was still oriented
around the idea that it did.  This commit fixes up the logging to make
sense again.

This doesn't fix an actual bug that causes problems, but it does fix a
conceptual error.

Fixes: 2301f7ebc15e ("tunnel: Make tnl_port_receive() parameter 'const'.")
Signed-off-by: Ben Pfaff <blp@ovn.org>
Reviewed-by: Yifeng Sun <pkusunyifeng@gmail.com>

ofproto/tunnel.c
@@ -301,7 +301,6 @@ tnl_port_del(const struct ofport_dpif *ofport, odp_port_t odp_port)
 const struct ofport_dpif *
 tnl_port_receive(const struct flow *flow) OVS_EXCLUDED(rwlock)
 {
-    char *pre_flow_str = NULL;
     const struct ofport_dpif *ofport;
     struct tnl_port *tnl_port;
 
@@ -318,20 +317,11 @@ tnl_port_receive(const struct flow *flow) OVS_EXCLUDED(rwlock)
     }
 
     if (!VLOG_DROP_DBG(&dbg_rl)) {
-        pre_flow_str = flow_to_string(flow, NULL);
-    }
-
-    if (pre_flow_str) {
-        char *post_flow_str = flow_to_string(flow, NULL);
+        char *flow_str = flow_to_string(flow, NULL);
         char *tnl_str = tnl_port_fmt(tnl_port);
-        VLOG_DBG("flow received\n"
-                 "%s"
-                 " pre: %s\n"
-                 "post: %s",
-                 tnl_str, pre_flow_str, post_flow_str);
+        VLOG_DBG("tunnel port %s receive from flow %s", tnl_str, flow_str);
         free(tnl_str);
-        free(pre_flow_str);
-        free(post_flow_str);
+        free(flow_str);
     }
 
 out: