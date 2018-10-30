openvswitch/ovs

commit b47e7e2bac7f21f8c81aaeb1068598a6064c348b

ofp-table: Always format the table number in table features.
Table features should indicate the table number as well as the table
name.  Before this, the first line for each table looked like this:
   table myname ("myname"):
but it's more useful if it's:
   table 123 ("myname"):

Signed-off-by: Ben Pfaff <blp@ovn.org>
Acked-by: Justin Pettit <jpettit@ovn.org>

lib/ofp-table.c
@@ -1432,7 +1432,6 @@ ofputil_table_features_format(
     const struct ofputil_table_features *prev_features,
     const struct ofputil_table_stats *stats,
     const struct ofputil_table_stats *prev_stats,
-    const struct ofputil_table_map *table_map,
     int *first_ditto, int *last_ditto)
 {
     int table = features->table_id;
@@ -1452,8 +1451,7 @@ ofputil_table_features_format(
     ofputil_table_features_format_finish(s, *first_ditto, *last_ditto);
     *first_ditto = -1;
 
-    ds_put_format(s, "\n  table ");
-    ofputil_format_table(table, table_map, s);
+    ds_put_format(s, "\n  table %"PRIu8, table);
     if (features->name[0]) {
         ds_put_format(s, " (\"%s\")", features->name);
     }

include/openvswitch/ofp-table.h
@@ -276,7 +276,6 @@ void ofputil_table_features_format(
     const struct ofputil_table_features *prev_features,
     const struct ofputil_table_stats *stats,
     const struct ofputil_table_stats *prev_stats,
-    const struct ofputil_table_map *table_map,
     int *first_ditto, int *last_ditto);
 void ofputil_table_features_format_finish(struct ds *,
                                           int first_ditto, int last_ditto);

lib/ofp-print.c
@@ -226,8 +226,7 @@ ofp_print_get_config_reply(struct ds *string, const struct ofp_header *oh)
 }
 
 static enum ofperr
-ofp_print_table_features_reply(struct ds *s, const struct ofp_header *oh,
-                               const struct ofputil_table_map *table_map)
+ofp_print_table_features_reply(struct ds *s, const struct ofp_header *oh)
 {
     struct ofpbuf b = ofpbuf_const_initializer(oh, ntohs(oh->length));
 
@@ -244,7 +243,7 @@ ofp_print_table_features_reply(struct ds *s, const struct ofp_header *oh,
         }
 
         ofputil_table_features_format(s, &tf, i ? &prev : NULL, NULL, NULL,
-                                      table_map, &first_ditto, &last_ditto);
+                                      &first_ditto, &last_ditto);
         prev = tf;
     }
 }
@@ -567,8 +566,7 @@ ofp_print_ofpst_port_reply(struct ds *string, const struct ofp_header *oh,
 }
 
 static enum ofperr
-ofp_print_table_stats_reply(struct ds *string, const struct ofp_header *oh,
-                            const struct ofputil_table_map *table_map)
+ofp_print_table_stats_reply(struct ds *string, const struct ofp_header *oh)
 {
     struct ofpbuf b = ofpbuf_const_initializer(oh, ntohs(oh->length));
     ofpraw_pull_assert(&b);
@@ -591,7 +589,7 @@ ofp_print_table_stats_reply(struct ds *string, const struct ofp_header *oh,
         ofputil_table_features_format(string,
                                       &features, i ? &prev_features : NULL,
                                       &stats, i ? &prev_stats : NULL,
-                                      table_map, &first_ditto, &last_ditto);
+                                      &first_ditto, &last_ditto);
         prev_features = features;
         prev_stats = stats;
     }
@@ -991,7 +989,7 @@ ofp_to_string__(const struct ofp_header *oh,
 
     case OFPTYPE_TABLE_FEATURES_STATS_REQUEST:
     case OFPTYPE_TABLE_FEATURES_STATS_REPLY:
-        return ofp_print_table_features_reply(string, oh, table_map);
+        return ofp_print_table_features_reply(string, oh);
 
     case OFPTYPE_TABLE_DESC_REQUEST:
     case OFPTYPE_TABLE_DESC_REPLY:
@@ -1114,7 +1112,7 @@ ofp_to_string__(const struct ofp_header *oh,
         return ofp_print_ofpst_port_reply(string, oh, port_map, verbosity);
 
     case OFPTYPE_TABLE_STATS_REPLY:
-        return ofp_print_table_stats_reply(string, oh, table_map);
+        return ofp_print_table_stats_reply(string, oh);
 
     case OFPTYPE_AGGREGATE_STATS_REPLY:
         return ofp_print_aggregate_stats_reply(string, oh);

utilities/ovs-ofctl.c
@@ -926,7 +926,6 @@ ofctl_dump_table_features(struct ovs_cmdl_context *ctx)
 
                     ofputil_table_features_format(
                         &s, &tf, n ? &prev : NULL, NULL, NULL,
-                        tables_to_show(ctx->argv[1]),
                         &first_ditto, &last_ditto);
 
                     prev = tf;

ykulazhenkov/ovn

commit 04cec149a3b974cdfde3a12da1d86e40c9ae1484

ofp-table: Fix table print format string.
This fixes travis build on OSX:

https://travis-ci.org/openvswitch/ovs/jobs/446920531

lib/ofp-table.c:1454:42: error: \
format specifies type 'unsigned char' but the argument has type 'int'
    ds_put_format(s, "\n  table %"PRIu8, table);
                                ~~       ^~~~~

CC: Ben Pfaff <blp@ovn.org>
Fixes: b47e7e2bac7f ("ofp-table: Always format the table number in table features.")
Signed-off-by: Ilya Maximets <i.maximets@samsung.com>
Signed-off-by: Ben Pfaff <blp@ovn.org>

lib/ofp-table.c
@@ -1451,7 +1451,7 @@ ofputil_table_features_format(
     ofputil_table_features_format_finish(s, *first_ditto, *last_ditto);
     *first_ditto = -1;
 
-    ds_put_format(s, "\n  table %"PRIu8, table);
+    ds_put_format(s, "\n  table %d", table);
     if (features->name[0]) {
         ds_put_format(s, " (\"%s\")", features->name);
     }