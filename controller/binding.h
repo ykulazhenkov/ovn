936993fe1b73f2121112efb442d53d1edbf7fcf6
Numan Siddique <numans@ovn.org>
2020-01-22 17:33:49 +0530

    ovn-controller: I-P for SB port binding and OVS interface in runtime_data.
    This patch handles SB port binding changes and OVS interface changes
    incrementally in the runtime_data stage which otherwise would have
    resulted in calls to binding_run().
    
    Prior to this patch, port binding changes were handled differently.
    The changes were only evaluated to see if they need full recompute
    or they can be ignored.
    
    With this patch, the runtime data is updated with the changes (both
    SB port binding and OVS interface) and ports are claimed/released in
    the handlers itself, avoiding the need to trigger recompute of runtime
    data stage.
    
    Acked-by: Mark Michelson <mmichels@redhat.com>
    Signed-off-by: Numan Siddique <numans@ovn.org>

--- controller/binding.h
+++ controller/binding.h
@@ -57,6 +57,7 @@ struct binding_ctx_out {
     struct sset *local_lports;
     struct sset *local_lport_ids;
     struct sset *egress_ifaces;
+    struct smap *local_iface_ids;
 };
 
 void binding_register_ovs_idl(struct ovsdb_idl *);
@@ -64,9 +65,13 @@ void binding_run(struct binding_ctx_in *, struct binding_ctx_out *);
 bool binding_cleanup(struct ovsdb_idl_txn *ovnsb_idl_txn,
                      const struct sbrec_port_binding_table *,
                      const struct sbrec_chassis *);
-bool binding_evaluate_port_binding_changes(struct binding_ctx_in *,
-                                           struct binding_ctx_out *);
 
 void local_bindings_init(struct shash *local_bindings);
 void local_bindings_destroy(struct shash *local_bindings);
+bool binding_handle_ovs_interface_changes(struct binding_ctx_in *,
+                                          struct binding_ctx_out *,
+                                          bool *changed);
+bool binding_handle_port_binding_changes(struct binding_ctx_in *,
+                                         struct binding_ctx_out *,
+                                         bool *changed);
 #endif /* controller/binding.h */
